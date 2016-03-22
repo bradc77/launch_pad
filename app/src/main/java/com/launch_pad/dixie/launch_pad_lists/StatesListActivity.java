ef4278025fc8087097a31d57e6f53b8b12399c01
bradc77 <bradc@cabosoft.com>
2016-03-22 17:19:26 -0600

    add flag images

--- app/src/main/java/com/launch_pad/dixie/launch_pad_lists/StatesListActivity.java
+++ app/src/main/java/com/launch_pad/dixie/launch_pad_lists/StatesListActivity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class StatesListActivity extends AppCompatActivity
 {
+	private ListView mStatesListView;
+	private ArrayList<String> mStateData;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -16,6 +21,12 @@ public class StatesListActivity extends AppCompatActivity
 		setContentView(R.layout.activity_states_list);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+
+		setupData();
+
+		mStatesListView = (ListView) findViewById(R.id.states_list_view);
+
+		mStatesListView.setAdapter(new StatesListAdapter(StatesListActivity.this, mStateData));
 	}
 
 	@Override
@@ -41,5 +52,25 @@ public class StatesListActivity extends AppCompatActivity
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void setupData()
+	{
+		String [] names = { "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
+				"Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+				"Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+				"Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+				"Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+				"Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" };
+
+		if (mStateData == null)
+		{
+			mStateData = new ArrayList<String>();
+		}
+
+		for (String thisName : names)
+		{
+			mStateData.add(thisName);
+		}
 	}
 }
